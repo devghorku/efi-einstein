084dcda9bce6add686510236318552b4d97702ae
devghorku <101194238+devghorku@users.noreply.github.com>
2022-04-05 10:28:52 +0600

    axios call

--- src/store/index.js
+++ src/store/index.js
@@ -6,9 +6,39 @@ Vue.use(Vuex)
 
 /* eslint-disable no-new */
 const store = new Vuex.Store({
-  plugins: [createPersistedState()],
-  modules: {
-  }
+    plugins: [createPersistedState()],
+    state() {
+        return {
+            poolList: []
+        }
+    },
+    getters: {
+        pools: state => state.poolList
+    },
+    mutations: {
+        set_pool(state, payload) {
+            state.poolList = payload
+        }
+    },
+    actions: {
+        async getPool({commit}) {
+            try {
+                const res = await this.axios.get('poolpairs')
+                commit('set_pool', res.data)
+            } catch (e) {
+
+            }
+        },
+        async getPrice({commit},token) {
+            try {
+                const res = await this.axios.get('prices/'+token+'-USD')
+                return res.data
+            } catch (e) {
+
+            }
+        }
+    },
+    modules: {}
 })
 
 export default store
