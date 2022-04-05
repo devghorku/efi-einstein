1e779b939198358e48a16cf8be193e89d04b54c0
Sharif Minhazul Abeden <56829132+sharifminhaz@users.noreply.github.com>
2022-04-05 12:01:17 +0600

    footer separated

---
+++ src/components/footerApp.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-footer class="white my-6">
+    <div class="ml-10 d-flex">
+      <v-icon class="mx-2">
+        mdi-twitter
+      </v-icon>
+      <v-icon class="mx-2">
+        mdi-instagram
+      </v-icon>
+      <v-icon class="mx-2">
+        mdi-facebook
+      </v-icon>
+    </div>
+    <v-spacer></v-spacer>
+    <div>Address:xxxxxx</div>
+    <v-spacer></v-spacer>
+    <div class="mr-10">&copy; copyright 2022</div>
+  </v-footer>
+</template>
+<script>
+export default {
+  name:'footerApp',
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import axios from "axios";
+import  "./assets/app.scss"
 axios.defaults.baseURL = 'https://ocean.defichain.com/v0/mainnet/';
 Vue.config.productionTip = false
 
